994f28a7a31168601501c33ef43e373949681c8c
itsuka-to <50649356+itsuka-to@users.noreply.github.com>
2023-12-21 15:59:06 +0900

    add ros

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <std_msgs/msg/String.hpp>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    std_msgs::msg::String msg;
+    msg.data = "Hello, World!";
 
     return 0;
 }
